abfc54f15a9e50b59404eae17a237ba232dc0b33
Melissa <Melissa@LAPTOP-MELI->
2024-03-09 01:25:47 -0400

    Agregar archivos de proyecto.

---
+++ CapaNegocios/AccionesBases.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos.Base_de_Datos;
+using System.Data;
+
+namespace CapaNegocios
+{
+    public class AccionesBases 
+    {
+        public Db_GoldDataContext dbGold = new Db_GoldDataContext();
+    }
+}
---
+++ Prueba Proyecto Programación/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prueba_Proyecto_Programación.MvcApplication" Language="C#" %>
