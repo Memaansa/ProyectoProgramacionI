ab00581fa0b0aa483e0c879931aeb373b3ed5dbf
Yamel Miranda <yamelmiranda@yahoo.com>
2024-03-16 13:20:06 -0400

    cambios video 5

---
+++ Prueba Proyecto Programación/Controllers/MantenimientoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prueba_Proyecto_Programación.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        // GET: Mantenimiento
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
