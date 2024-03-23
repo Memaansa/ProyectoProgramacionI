82ce00dafd7afc692692f0d5d3378792e4786a36
melissamarie andujar sanz <mandujar7@est.unibe.edu.do>
2024-03-23 15:13:06 -0400

    Ultimos arreglos

---
+++ CapaModelo/Modelos/ModelCategory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos.Base_de_Datos;
+
+namespace CapaModelo.Modelos
+{
+    public class ModelCategory
+    {
+        public int idCategory {  get; set; }
+
+        public string _descriptionCategory { get; set; }
+
+        public DateTime _dateRegister { get; set; }
+
+        public bool _flag {  get; set; }
+
+        public ModelCategory()
+        {
+
+        }
+
+        public ModelCategory(int _idCategory,string _descriptionCategory,DateTime _dataRegister, bool _falg)
+        {
+            this.idCategory = _idCategory;
+            this._descriptionCategory = _descriptionCategory;
+            this._dateRegister = _dataRegister;
+            this._flag = _falg;
+
+         
+        }
+    }
+}
--- Prueba Proyecto Programación/Controllers/MantenimientoController.cs
+++ Prueba Proyecto Programación/Controllers/MantenimientoController.cs
@@ -8,17 +8,13 @@ using System.Web.Mvc;
 namespace Prueba_Proyecto_Programación.Controllers
 {
     public class MantenimientoController : Controller
-
-        public AccionMantenimientos mantenimientos = new AccionMantenimientos();
     {
         // GET: Mantenimiento
-        public ActionResult MantenimientoIndex()
+        public ActionResult Index()
         {
-            List<mEstatus> lista_estados = new List<mEstatus>();
-            lista_estados = mantenimientos.ListarEstados();
+            
 
-
-            return View(lista_estados);
+            return View();
         }       
     }
 }
