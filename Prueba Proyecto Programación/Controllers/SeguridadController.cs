d3f888e9e84829a4d6e6787f0ce6a36d5ee5768f
melissamarie andujar sanz <mandujar7@est.unibe.edu.do>
2024-03-22 23:20:29 -0400

    Cambios sobre el login

---
+++ Prueba Proyecto Programación/Controllers/SeguridadController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prueba_Proyecto_Programación.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        // GET: Seguridad
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+    }
+}
