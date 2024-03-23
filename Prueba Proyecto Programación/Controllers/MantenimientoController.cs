6a8d1115c0e4f2e4a2cb329fac51d54c688fd939
melissamarie andujar sanz <mandujar7@est.unibe.edu.do>
2024-03-23 14:13:10 -0400

    Clase del lunes 18 de marzo

--- Prueba Proyecto Programación/Controllers/MantenimientoController.cs
+++ Prueba Proyecto Programación/Controllers/MantenimientoController.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Prueba_Proyecto_Programación.Controllers
 {
     public class MantenimientoController : Controller
+
+        public AccionMantenimientos mantenimientos = new AccionMantenimientos();
     {
         // GET: Mantenimiento
-        public ActionResult Index()
+        public ActionResult MantenimientoIndex()
         {
-            return View();
-        }
+            List<mEstatus> lista_estados = new List<mEstatus>();
+            lista_estados = mantenimientos.ListarEstados();
+
+
+            return View(lista_estados);
+        }       
     }
 }
