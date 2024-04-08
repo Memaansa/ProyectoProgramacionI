6441a113941b3d4cc747648e3beb98a2cb650e04
IanYarullMarte <i.yarull1@est.unibe.edu.do>
2024-04-08 15:10:12 -0400

    Clase del Miercoles, creacion de TM_OperacionesSistema, vistas de mantenimiento, guardar operaciones.

--- CapaNegocios/Acciones/AcciónMantenimiento.cs
+++ CapaNegocios/Acciones/AcciónMantenimiento.cs
@@ -10,8 +10,43 @@ namespace CapaNegocios.Acciones
 {
     public class AcciónMantenimiento : AccionesBases
     {
-        
+        public string GuardarOperacionSistema(string name, string url)
+        {
+            //mensaje de retorno
 
-        
+            string msg;
+
+            try
+            {
+                //bloque de codigo para llamar las operaciones del sistema
+                TM_OperacionSistema register = new TM_OperacionSistema()
+                {
+                    DescripcionOperacion = name,
+                    enlace_OperacionSistema = url,
+                    fecCreacion = DateTime.Now,
+                    usuarioCreacion = Environment.UserName,
+                    flagActivo = true
+                };
+
+                msg = "La informacion fue registrada correctamente";
+
+                dbGold.TM_OperacionSistemas.InsertOnSubmit(register);
+                dbGold.SubmitChanges(); //commit
+                
+            }
+            catch (Exception e)
+            {
+                msg = $"Hubo un problema en la operacion{e.Message}";
+            }
+
+            return msg;
+
+        }
+
+
+
     }
+
+
 }
+
--- Prueba Proyecto Programación/Controllers/MantenimientoController.cs
+++ Prueba Proyecto Programación/Controllers/MantenimientoController.cs
@@ -11,12 +11,18 @@ namespace Prueba_Proyecto_Programación.Controllers
     public class MantenimientoController : Controller
     {
         public AcciónMantenimiento Mantenimiento = new AcciónMantenimiento();
+
+
+
         // GET: Mantenimiento
         public ActionResult Mantenimientoindex()
         {
-           
-
             return View();
-        }       
+        }
+
+        public string GuardarOperacion(string descripcion, string ruta)
+        {
+            return Mantenimiento.GuardarOperacionSistema(descripcion, ruta);
+        }
     }
 }
